8d77ddf496da944c226691d0ae1999d9233bd870
Bruno Ortiz <brunoort@gmail.com>
2019-09-16 10:02:45 -0300

    controller changes

--- .history/src/server_20190913114513.ts
+++ .history/src/server_20190913114513.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 
-import { ApplicationModule } from './app/app.module';
+import { ApplicationModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
